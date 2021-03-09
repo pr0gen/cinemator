b5263a108629eab087abcbc4c06cc66961d85717
Rolfie <52386098+Rolf1e@users.noreply.github.com>
2021-03-09 16:48:25 +0100

    CICD for server (#2)

--- server/src/app.controller.spec.ts
+++ server/src/app.controller.spec.ts
@@ -16,7 +16,8 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      let text = appController.getHello();
+      expect(text['text']).toBe('hello worl');
     });
   });
 });
