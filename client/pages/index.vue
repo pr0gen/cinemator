705d7fc642a1586ac01bfc974dcb1343a6eb0d42
Arthur <arthur.remond@protonmail.com>
2021-03-25 11:10:17 +0100

    add : index Page + Card Component + search Movie

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,9 +1,30 @@
 <template>
-  <section class='section'>
-    <div class='columns is-mobile'>
-      <h1> Je suis l'index </h1>
-    </div>
+
+  <section class='movie container mt-6'>
+
+    <b-field class='mb-6'>
+      <b-input placeholder="Search..."
+               type="search"
+               icon="magnify"
+               @keyup.enter.native="search"
+               v-model="searchValue"
+               icon-clickable>
+      </b-input>
+    </b-field>
+
+
+    <Card
+      :active="active"
+      v-for="movie in movies"
+      :key="movie.id"
+      :title='movie.title'
+      :description='movie.description'
+      :image='movie.image'
+    >
+
+    </Card>
   </section>
+
 </template>
 
 <script>
@@ -12,8 +33,29 @@ import Card from '~/components/Card';
 export default {
   name: 'HomePage',
 
+  async asyncData({ $axios }) {
+    const movies = await $axios.$get('http://localhost:3000/imdb/search?name=avenger')
+    return { movies: movies.results }
+  },
+
+  methods: {
+    async search() {
+      this.active = false
+      const {data : movies} = await this.$axios.get(`http://localhost:3000/imdb/search?name=${this.searchValue}`)
+      this.movies = movies.results
+      this.active = true
+    }
+  },
+
   components: {
     Card,
+  },
+  data() {
+    return {
+      movies: [],
+      active: true,
+      searchValue : '',
+    };
   },
 };
 </script>
