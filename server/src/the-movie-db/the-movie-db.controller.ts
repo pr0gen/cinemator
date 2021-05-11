084ab2b34da0e45d270079b4a12d56b6b66e8ccc
Tigran Slama <52386098+Rolf1e@users.noreply.github.com>
2021-05-11 14:13:36 +0200

    movie details  (#17)

--- server/src/the-movie-db/the-movie-db.controller.ts
+++ server/src/the-movie-db/the-movie-db.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { SearchResult } from './dto';
+import { SearchResult, MovieDetails } from './dto';
 import { TheMovieDbService } from './the-movie-db.service';
 
 @Controller('the-movie-db')
@@ -7,13 +7,19 @@ export class TheMovieDbController {
 
   constructor(private theMovieDbService: TheMovieDbService) {}
 
-  @Get('search')
-  async findOne(@Query('expression') expression: string): Promise<SearchResult| Error> {
-    return this.theMovieDbService.search_by_expression(expression)
-      .toPromise()
-      .then(res => res.data)
-      .catch(err => new Error(err));
-  }
+    @Get('search')
+    async findOne(@Query('expression') expression: string): Promise<SearchResult | Error> {
+        return this.theMovieDbService.search_by_expression(expression);
+    }
 
+    @Get('lang-details')
+    async lang_details(@Query('id') id: number): Promise<string[] | Error> {
+        return this.theMovieDbService.lang_details(id);
+    }
+
+    @Get('find-movie-details')
+    async find_movie_details(@Query('id') id: number): Promise<MovieDetails | Error> {
+        return this.theMovieDbService.find_movie_details(id);
+    }
 
 }
--- server/src/the-movie-db/the-movie-db.service.ts
+++ server/src/the-movie-db/the-movie-db.service.ts
@@ -1,9 +1,7 @@
 import { HttpService, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { AxiosResponse } from 'axios'
 import { Connection } from '../the-movie-db/connection';
 import * as ApiConstant from './constants';
-import { SearchResult } from './dto';
+import { MovieTranslation, SearchResult, MovieDetails} from './dto';
 import { ApiAuthService } from '../api-auth/api-auth.service';
 
 @Injectable()
@@ -17,9 +15,39 @@ constructor(
         this.connection = authService.get_connection();
     }
 
-    search_by_expression(expression: string): Observable<AxiosResponse<SearchResult>> {
-      return this.httpService
-        .get(ApiConstant.BASIC_SEARCH + this.connection.api_key_the_movie_db + ApiConstant.QUERY + expression);
+    async search_by_expression(expression: string): Promise<SearchResult | Error> {
+          return this.httpService
+              .get(ApiConstant.BASIC_SEARCH + this.connection.api_key_the_movie_db + ApiConstant.QUERY + expression)
+              .toPromise()
+              .then(res => res.data)
+              .catch(err => new Error(err));
+      }
+
+    async lang_details(id: number): Promise<string[] | Error> {
+        return this.httpService
+            .get(
+                ApiConstant.BASE_LANG_DETAILS + id + '/' + ApiConstant.TRANSLATION +
+                ApiConstant.API_KEY_PATH + this.connection.api_key_the_movie_db
+            )
+            .toPromise()
+            .then(res => {
+                let data: MovieTranslation = res.data;
+                return data.translations.map(t => t.name);
+            })
+            .catch(err => new Error(err));
+    }
+
+    async find_movie_details(id: number): Promise<MovieDetails | Error> {
+            console.log(ApiConstant.MOVIE_DETAILS + id + '/' + ApiConstant.API_KEY_PATH 
+            + this.connection.api_key_the_movie_db + ApiConstant.LANG + "en-US");
+        return this.httpService
+            .get(
+              ApiConstant.MOVIE_DETAILS + id + ApiConstant.API_KEY_PATH 
+              + this.connection.api_key_the_movie_db + ApiConstant.LANG + "en-US"
+            )
+            .toPromise()
+            .then(res => res.data)
+            .catch(err => new Error(err));
     }
 
 }
