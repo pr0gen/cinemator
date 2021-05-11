e07484f8796729b26d8681b014fb099ae79437ba
qroyer <qroyer@adexos.fr>
2021-05-11 16:29:06 +0200

    fix spec dependencies

--- server/src/the-movie-db/the-movie-db.controller.spec.ts
+++ server/src/the-movie-db/the-movie-db.controller.spec.ts
@@ -1,18 +1,24 @@
-import { HttpModule } from '@nestjs/common';
+import { HttpModule, UseInterceptors, CacheModule, CacheInterceptor } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TheMovieDbController } from './the-movie-db.controller';
 import { TheMovieDbService } from './the-movie-db.service';
 import { ApiAuthService } from '../api-auth/api-auth.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 describe('TheMovieDbController', () => {
   let controller: TheMovieDbController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [HttpModule, ConfigModule],
+      imports: [HttpModule, ConfigModule , CacheModule.register()],
       controllers: [TheMovieDbController],
-      providers: [TheMovieDbService, ApiAuthService]
+      providers: [TheMovieDbService, ApiAuthService,
+        {
+          provide: APP_INTERCEPTOR,
+          useClass: CacheInterceptor,
+        },
+      ],
     }).compile();
 
     controller = module.get<TheMovieDbController>(TheMovieDbController);
