d73322d4ee018729848d2311afa499e78b697c28
qroyer <qroyer@adexos.fr>
2021-05-11 16:37:58 +0200

    fix configmodule dependencies

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,13 +1,18 @@
-import {Module, CacheModule, CacheInterceptor} from '@nestjs/common';
+import { Module, CacheModule, CacheInterceptor } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './user.entity';
 import { ApiAuthService } from '../api-auth/api-auth.service';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), CacheModule.register()],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    CacheModule.register(),
+    ConfigModule.forRoot(),
+  ],
   providers: [
     UsersService,
     ApiAuthService,
